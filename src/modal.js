daaef562975ac73829df9573cd6a6f9c0dd05abb
LeKawa <hector.travaille@hetic.net>
2018-03-25 13:05:20 +0200

    modal added

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,6 @@
-var btn = document.querySelector('button');
+var btn = document.querySelector('.header-container__menu');
 var modal = document.querySelector('.modal');
-var overlay = document.querySelector('.modal_overlay');
+var overlay = document.querySelector('.modal-overlay');
 
 btn.addEventListener('click', function() {
   modal.style.display = 'block';
