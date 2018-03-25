188171f1e288da129bd887dafeacad61e59192c8
LeKawa <hector.travaille@hetic.net>
2018-03-25 14:28:46 +0200

    back added

--- src/modal.js
+++ src/modal.js
@@ -1,11 +1,18 @@
 var btn = document.querySelector('.header-container__menu');
 var modal = document.querySelector('.modal');
+var back = document.querySelector('.header-container__menu__link__back');
 var overlay = document.querySelector('.modal-overlay');
+var contact = document.querySelector('.header-container__menu__link');
 
 btn.addEventListener('click', function() {
   modal.style.display = 'block';
+  back.style.display = 'block';
+  contact.style.display = 'none';
+
 });
 
 overlay.addEventListener('click', function() {
   modal.style.display = '';
+  back.style.display = '';
+  contact.style.display = 'block';
 });
