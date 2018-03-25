c9b3fd284908c80100531ae7dacb34daffd203a2
LeKawa <hector.travaille@hetic.net>
2018-03-25 14:42:18 +0200

    oui

--- src/modal.js
+++ src/modal.js
@@ -4,14 +4,14 @@ var back = document.querySelector('.header-container__menu__link__back');
 var overlay = document.querySelector('.modal-overlay');
 var contact = document.querySelector('.header-container__menu__link');
 
-btn.addEventListener('click', function() {
+contact.addEventListener('click', function() {
   modal.style.display = 'block';
   back.style.display = 'block';
   contact.style.display = 'none';
 
 });
 
-overlay.addEventListener('click', function() {
+back.addEventListener('click', function() {
   modal.style.display = '';
   back.style.display = '';
   contact.style.display = 'block';
